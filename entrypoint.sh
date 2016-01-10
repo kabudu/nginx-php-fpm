2c48f87c56618d19f25aad5839662ce864728055
kabudu <kabudu@gmail.com>
2016-01-10 08:21:42 -0500

    tweaking the entrypoint script a little

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,12 @@
 mkdir -p -m 0700 /root/.ssh
 echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
 
+# Display PHP error's or not
+if [[ "$ERRORS" != "1" ]] ; then
+  sed -i -e "s/error_reporting =.*=/error_reporting = E_ALL/g" /etc/php/php.ini
+  sed -i -e "s/display_errors =.*/display_errors = On/g" /etc/php/php.ini
+fi
+
 # Tweak nginx to match the workers to cpu's
 procs=$(cat /proc/cpuinfo | grep processor | wc -l)
 sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
@@ -20,6 +26,9 @@ if [[ "$TEMPLATE_NGINX_HTML" != "0" ]] ; then
     fi
   done
 fi
+
+# Again set the right permissions (needed when mounting from a volume)
+chown -Rf nginx.nginx /usr/share/nginx/html/
 
 # Start supervisord and services
 /usr/bin/supervisord -n -c /etc/supervisord.conf
